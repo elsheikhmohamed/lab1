4390d08de11f5f2f314851846d4366afc28c986b
elsheikhmohamed <elshikhmohamed091@gmail.com>
2024-12-01 01:03:50 -0800

    as
    as

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -14,7 +14,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        int x=6;
+System.out.println(x);    }
     
 }
