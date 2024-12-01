83369e2dfcb338a0aea657bcf2b7941abf54c7dc
elsheikhmohamed <elshikhmohamed091@gmail.com>
2024-12-01 01:12:39 -0800

    finish class person

---
+++ src/javaapplication2/person.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author al-aqsa
+ */
+public class person {
+    int age;
+    person(int age){
+        this.age=age;
+    }
+    
+}
